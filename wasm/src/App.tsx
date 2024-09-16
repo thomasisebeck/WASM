3039639df4bde8c7949d5160a2646e892d8bcea8
thomasisebeck <tisebeck@proton.me>
2024-09-16 22:41:39 +0200

    loading wasm file works

--- wasm/src/App.tsx
+++ wasm/src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useEffect, useRef, useState} from "react";
+import init from './wasm/adder_wasm.wasm?init'
+
+interface Params {
+    a: number,
+    b: number
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [numbers, setNumbers] = useState<Params>({a: 0, b: 0})
+    const [result, setResult] = useState<number>(0)
+    const adder = useRef<CallableFunction | null>(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        const loadWasm = () => {
+            init().then((instance) => {
+                adder.current = instance.exports.adder as CallableFunction;
+            })
+        }
+        loadWasm();
+    }, []);
+
+    const handleClick = () => {
+        setResult(adder.current!(numbers.a, numbers.b))
+    }
+
+    return (
+        <>
+            <input type={"number"} onChange={e => setNumbers({...numbers, a: Number(e.target.value)})}/>
+            <input type={"number"} onChange={e => setNumbers({...numbers, b: Number(e.target.value)})}/>
+            <br/>
+            <button onClick={handleClick}>get result</button>
+            <p>Result: {result}</p>
+        </>
+    )
 }
 
 export default App
